9b9964cfd7f58b079966cbc247c39277aeb66341
Deepak Tiwari <deepak.tiwari.1493@gmail.com>
2017-11-05 14:20:01 +0530

    add timestamps

--- db/migrate/20171105072217_create_users.rb
+++ db/migrate/20171105072217_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :type, null: false
+      t.timestamps
     end
 
     add_index :users, [:type, :id]

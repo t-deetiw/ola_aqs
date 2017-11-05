03cebf22a78cebeece86a1d634ea75123743ec6d
Deepak Tiwari <deepak.tiwari.1493@gmail.com>
2017-11-05 16:45:05 +0530

    more index

--- db/migrate/20171105075637_create_ride_requests.rb
+++ db/migrate/20171105075637_create_ride_requests.rb
@@ -13,5 +13,6 @@ class CreateRideRequests < ActiveRecord::Migration[5.0]
     add_index :ride_requests, :customer_id
     add_index :ride_requests, [:driver_id, :status_cd]
     add_index :ride_requests, :status_cd
+    add_index :ride_requests, :requested_at
   end
 end

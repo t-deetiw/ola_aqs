c9bc76004e5336f06833221649240d64ead9ec10
Deepak Tiwari <deepak.tiwari.1493@gmail.com>
2017-11-05 13:20:56 +0530

    Create seed to populate drivers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Drivers
+
+MAX_DRIVER_COUNT.times.each do |i|
+  Driver.create!
+end
